ba45620f965996889796b4e3b48e06f56e988816
Karol Katerzawa <qiubix@gmail.com>
2015-10-05 21:57:13 +0200

    Remove loop duplication

--- BowlingGameTest.cpp
+++ BowlingGameTest.cpp
@@ -6,20 +6,19 @@ using namespace testing;
 class BowlingGameTest : public Test {
 public:
   Game game;
+  void rollMany(int n, int pins) {
+    for (int i = 0; i < n; i++) {
+      game.roll(pins);
+    }
+  }
 };
 
 TEST_F(BowlingGameTest, testGutterGame) {
-  int pins = 0;
-  int n = 20;
-  for (int i = 0; i < n; i++) {
-    game.roll(pins);
-  }
+  rollMany(20, 0);
   ASSERT_THAT(game.score(), Eq(0));
 }
 
 TEST_F(BowlingGameTest, testAllOnes) {
-  for (int i = 0; i < 20; i++) {
-    game.roll(1);
-  }
+  rollMany(20, 1);
   ASSERT_THAT(game.score(), Eq(20));
 }
