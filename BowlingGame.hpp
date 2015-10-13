53799a838f6a3bdba11b96690b622f10bac48578
Karol Katerzawa <qiubix@gmail.com>
2015-10-13 21:01:13 +0200

    Rename meaningless 'i' variable

--- BowlingGame.hpp
+++ BowlingGame.hpp
@@ -10,15 +10,15 @@ public:
 
   int score() {
     int totalScore = 0;
-    int i = 0;
+    int frameIndex = 0;
     for (int frame = 0; frame < 10; frame++) {
-      if (rolls[i] + rolls[i+1] == 10) {
-        totalScore += 10 + rolls[i+2];
-        i += 2;
+      if (rolls[frameIndex] + rolls[frameIndex+1] == 10) { //spare
+        totalScore += 10 + rolls[frameIndex+2];
+        frameIndex += 2;
       }
       else {
-        totalScore += rolls[i] + rolls[i+1];
-        i += 2;
+        totalScore += rolls[frameIndex] + rolls[frameIndex+1];
+        frameIndex += 2;
       }
     }
     return totalScore;
