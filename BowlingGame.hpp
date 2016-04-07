24158094b23a0c3cdea54df47ad9c8525b25885b
qiubix <qiubix@gmail.com>
2016-04-07 16:15:20 +0200

    Move by frames instead of rolls

--- BowlingGame.hpp
+++ BowlingGame.hpp
@@ -10,8 +10,10 @@ public:
 
   int score() {
     int totalScore = 0;
-    for (int i = 0; i < 21; ++i) {
-      totalScore += rolls[i];
+    int i = 0;
+    for (int frame = 0; frame < 10; ++frame) {
+      totalScore += rolls[i] + rolls[i+1];
+      i += 2;
     }
     return totalScore;
   }
