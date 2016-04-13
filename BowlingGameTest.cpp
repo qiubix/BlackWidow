c10815bc5e7dcc8f40e2609444573931e44c4b28
qiubix <qiubix@gmail.com>
2016-04-13 14:19:14 +0200

    First test for worst game
    clean up previous implementation

--- BowlingGameTest.cpp
+++ BowlingGameTest.cpp
@@ -3,50 +3,11 @@
 
 using namespace testing;
 
-class BowlingGameTest : public Test {
-public:
+TEST(BowlingGameTest, testGutterGame) {
   Game game;
-  void rollMany(int n, int pins) {
-    for (int i = 0; i < n; i++) {
-      game.roll(pins);
-    }
+  for (int i = 0; i < 20; ++i) {
+    game.roll(0);
   }
-  void rollSpare() {
-    game.roll(5);
-    game.roll(5);
-  }
-
-  void rollStrike() {
-    game.roll(10);
-  }
-};
-
-TEST_F(BowlingGameTest, testGutterGame) {
-  rollMany(20, 0);
   ASSERT_THAT(game.score(), Eq(0));
 }
 
-TEST_F(BowlingGameTest, testAllOnes) {
-  rollMany(20, 1);
-  ASSERT_THAT(game.score(), Eq(20));
-}
-
-TEST_F(BowlingGameTest, testOneSpare) {
-  rollSpare();
-  game.roll(3);
-  rollMany(17,0);
-  ASSERT_THAT(game.score(), Eq(16));
-}
-
-TEST_F(BowlingGameTest, testOneStrike) {
-  rollStrike();
-  game.roll(3);
-  game.roll(4);
-  rollMany(16, 0);
-  ASSERT_THAT(game.score(), Eq(24));
-}
-
-TEST_F(BowlingGameTest, testPerfectGame) {
-  rollMany(12, 10);
-  ASSERT_THAT(game.score(), Eq(300));
-}
