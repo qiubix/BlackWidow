9aa40f0754beece1e256fbbfb7382054062dcac9
qiubix <qiubix@gmail.com>
2016-04-13 14:41:27 +0200

    Handle spare, introduce table of rolls

--- BowlingGame.hpp
+++ BowlingGame.hpp
@@ -4,18 +4,26 @@
 class Game
 {
 public:
-  Game() : totalScore(0) {}
-
   void roll(int pins) {
-    totalScore += pins;
+    rolls[currentRoll++] = pins;
   }
 
   int score() {
+    int totalScore = 0;
+    for (int i = 0; i < 21; ++i) {
+      if (i >= 2 && rolls[i-2] + rolls[i-1] == 10) {
+        totalScore += 2 * rolls[i];
+      }
+      else {
+        totalScore += rolls[i];
+      }
+    }
     return totalScore;
   }
 
 private:
-  int totalScore;
+  int currentRoll = 0;
+  int rolls[21] = {};
 };
 
 #endif //BOWLING_HAME_HPP
