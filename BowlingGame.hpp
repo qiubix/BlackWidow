940bf9ec3960fe4528a52ab7898d3ec473bb3bbc
qiubix <qiubix@gmail.com>
2016-04-13 14:53:56 +0200

    Refactor: extract expresions to methods

--- BowlingGame.hpp
+++ BowlingGame.hpp
@@ -12,12 +12,12 @@ public:
     int totalScore = 0;
     int frameIndex = 0;
     for (int frame = 0; frame < 10; ++frame) {
-      if (rolls[frameIndex] + rolls[frameIndex+1] == 10) {
-        totalScore += 10 + rolls[frameIndex+2];
+      if (isSpare(frameIndex)) {
+        totalScore += 10 + spareBonus(frameIndex);
         frameIndex += 2;
       }
       else {
-        totalScore += rolls[frameIndex] + rolls[frameIndex+1];
+        totalScore += sumPinsInOneFrame(frameIndex);
         frameIndex += 2;
       }
     }
@@ -27,6 +27,18 @@ public:
 private:
   int currentRoll = 0;
   int rolls[21] = {};
+
+  bool isSpare(int frameIndex) const {
+    return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
+  }
+
+  int sumPinsInOneFrame(int frameIndex) {
+    return rolls[frameIndex] + rolls[frameIndex+1];
+  }
+
+  int spareBonus(int frameIndex) {
+    return rolls[frameIndex+2];
+  }
 };
 
 #endif //BOWLING_HAME_HPP
