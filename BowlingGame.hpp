6944fbda7a4b9beeced066953410cd76a41069d2
qiubix <qiubix@gmail.com>
2016-08-05 18:34:57 +0200

    Test zero-score game

--- BowlingGame.hpp
+++ BowlingGame.hpp
@@ -1,4 +1,16 @@
 #ifndef BOWLING_HAME_HPP
 #define BOWLING_HAME_HPP
 
+class Game
+{
+public:
+  void roll(int pins) {
+  }
+
+  int score() {
+    return 0;
+  }
+
+};
+
 #endif //BOWLING_HAME_HPP
--- BowlingGameTest.cpp
+++ BowlingGameTest.cpp
@@ -8,4 +8,9 @@ public:
 };
 
 TEST_F(BowlingGameTest, testGutterGame) {
+  Game game;
+  for (int i = 0; i < 20; i++) {
+    game.roll(0);
+  }
+  ASSERT_THAT(game.score(), Eq(0));
 }
